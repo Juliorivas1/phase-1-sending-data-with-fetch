f1631cb6c58be79f99ea41e26267581ac2452282
Julio Rivas <rivasjulio@ymail.com>
2023-07-05 20:20:14 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch('http://localhost:3000/users', configObj)
+      .then(response => response.json())
+      .then(data => {
+       
+        const id = data.id;
+        document.body.innerHTML += `<p>New user id: ${id}</p>`;
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }
+ 
+  
+  
